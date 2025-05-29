90ed2e8cfd1c482ea9ba90d84e647d9861c046db
jadeeloh <158065446+jadeeloh@users.noreply.github.com>
2025-05-30 09:14:51 +1000

    still doing layout changes and changedfont size , line height etc
    still doing layout changes and changedfont size , line height etc

---
+++ class-changes.js
@@ -0,0 +1,15 @@
+const classChanges = [
+  { time: 3, target: ".quote", add: "font-purple" },
+  { time: 9, target: ".quote", remove: "hide" },
+  { time: 9, target: ".page", add: "update" },
+  { time: 12, target: ".page", remove: "update" },
+
+
+  { time: 9, target: ".changeme2", add: "show" },
+  { time: 16, target: ".changeme2", remove: "show" },
+  { time: 16, target: ".changeme2", add: "hidden" },
+
+  { time: 16, target: ".changeme3", add: "show" },
+  { time: 28, target: ".changeme3", remove: "show" },
+  { time: 28, target: ".changeme3", add: "hidden" }
+];
