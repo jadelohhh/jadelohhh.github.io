cabb95827c6b127f419d2cc94ce58249b22808cf
jadeeloh <158065446+jadeeloh@users.noreply.github.com>
2025-06-08 01:36:35 +1000

    final changes made

--- class-changes.js
+++ class-changes.js
@@ -1,15 +1,35 @@
 const classChanges = [
-  { time: 3, target: ".quote", add: "font-purple" },
-  { time: 9, target: ".quote", remove: "hide" },
-  { time: 9, target: ".page", add: "update" },
-  { time: 3, target: ".pillpopup", add: "pillpopup" },
+
+  { time: 3, target: ".orb-verylg-1", add: "move" },
+  { time: 3, target: ".orb-verylg-2", add: "move" },
+  { time: 41, target: ".orb-verylg-1", remove: "move" },
+  { time: 41, target: ".orb-verylg-2", remove: "move" },
+//first mint orb 
+
+  { time: 42, target: ".page", add: "updateredbg" },
+  { time: 70, target: ".page", remove: "updateredbg" },
+  //bg becomes red at quote and chapter 2
+
+  { time: 148, target: ".orb-verylg-5", add: "move" },
+  { time: 148, target: ".orb-verylg-6", add: "move" },
+  { time: 207, target: ".orb-verylg-5", remove: "move" },
+  { time: 207, target: ".orb-verylg-6", remove: "move" },
+//red orb moves at chapter 3
+
+  { time: 208, target: ".page", add: "updategreenbg" },
+  { time: 266, target: ".page", remove: "updategreenbg" },
+  //update at chapter 4
 
 
-  { time: 9, target: ".changeme2", add: "show" },
-  { time: 16, target: ".changeme2", remove: "show" },
-  { time: 16, target: ".changeme2", add: "hidden" },
+  { time: 351, target: ".orb-lg-1", add: "move" },
+  { time: 351, target: ".orb-lg-2", add: "move" },
+  { time: 390, target: ".orb-lg-1", remove: "move" },
+  { time: 390, target: ".orb-lg-2", remove: "move" },
+  //red and purple orbs move away and back together
 
-  { time: 16, target: ".changeme3", add: "show" },
-  { time: 28, target: ".changeme3", remove: "show" },
-  { time: 28, target: ".changeme3", add: "hidden" }
+  { time: 1, target: ".floatupdown", add: "floating" },
+  { time: 1, target: ".floatupdownup", add: "floatingArmy" },
+
+
+
 ];
